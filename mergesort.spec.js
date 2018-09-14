991252d2db5dc67574e76795de9118bbeedab059
Gon Espeche <39110211+gonpeche@users.noreply.github.com>
2018-09-14 10:13:39 -0300

    Update mergesort.spec.js

--- mergesort.spec.js
+++ mergesort.spec.js
@@ -4,3 +4,20 @@ describe('Split Array function', function() {
       expect(split(arr)).toEqual([ [1,2,3] , [4,5] ])
     });
 });
+
+describe('Merge', function(){
+    it('es capaz de mergear dos arreglos ordenados a un solo arreglo ordenado', function(){
+        let arr1 = [2,5,9]
+        let arr2 = [3,4,8]
+    expect(merge(arr1,arr2)).toEqual([2,3,4,5,8,9])
+    });
+  });
+
+describe('MergeSort', function(){
+
+    it('se llama a si misma más de una vez', function(){
+        spyOn(window, 'mergeSort').and.callThrough();
+        mergeSort([4,5,7,34,54,3,18,16,21])
+        expect(mergeSort.calls.count()).toBeGreaterThan(1);
+    });
+});
